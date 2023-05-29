55c706eebf8bce97f266f37ddf858abcd38972ef
redpangilinan <redpangilinan715@gmail.com>
2023-05-29 20:40:41 +0800

    Integrated AnimeTable component

--- src/components/TopAnimeList.tsx
+++ src/components/TopAnimeList.tsx
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import axios from 'axios';
+import AnimeTable from './AnimeTable';
 
 type Anime = {
   mal_id: number;
@@ -77,65 +78,7 @@ const TopAnimeList: React.FC = () => {
   return (
     <div className='px-2'>
       <h1 className='text-2xl font-bold my-4'>Top Anime</h1>
-      <div className='overflow-x-auto w-full'>
-        <table className='min-w-full divide-y divide-gray-200'>
-          <thead className='bg-gray-100'>
-            <tr>
-              <th className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>
-                Rank
-              </th>
-              <th className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>
-                Image
-              </th>
-              <th className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>
-                Name
-              </th>
-              <th className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>
-                Date Aired
-              </th>
-              <th className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>
-                Episodes
-              </th>
-              <th className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>
-                Score
-              </th>
-            </tr>
-          </thead>
-          <tbody className='bg-white divide-y divide-gray-200'>
-            {topAnime.map((anime) => (
-              <tr key={anime.mal_id}>
-                <td className='px-6 py-4 whitespace-nowrap text-sm text-gray-500'>
-                  {anime.rank}
-                </td>
-                <td className='px-6 py-4 whitespace-nowrap'>
-                  {anime.images.jpg &&
-                  anime.images.jpg.image_url.endsWith('.jpg') ? (
-                    <img
-                      src={anime.images.jpg.image_url}
-                      alt={anime.title}
-                      className='w-16 h-auto object-cover rounded-lg'
-                    />
-                  ) : (
-                    <p className='text-red-500'>Invalid image URL</p>
-                  )}
-                </td>
-                <td className='px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900'>
-                  {anime.title}
-                </td>
-                <td className='px-6 py-4 whitespace-nowrap text-sm text-gray-500'>
-                  {anime.aired.string}
-                </td>
-                <td className='px-6 py-4 whitespace-nowrap text-sm text-gray-500'>
-                  {anime.episodes}
-                </td>
-                <td className='px-6 py-4 whitespace-nowrap text-sm text-gray-500'>
-                  {anime.score}
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+      <AnimeTable data={topAnime} />
 
       <div className='flex justify-center mt-4'>
         <nav
