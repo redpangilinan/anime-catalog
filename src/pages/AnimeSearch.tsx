96a56faaf0528118a52aa906a50001e3a0002994
redpangilinan <redpangilinan715@gmail.com>
2023-05-29 20:40:48 +0800

    Implemented anime search

--- src/pages/AnimeSearch.tsx
+++ src/pages/AnimeSearch.tsx
@@ -1,66 +1,63 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
+import AnimeTable from '../components/AnimeTable';
 
-interface Anime {
+type Anime = {
   mal_id: number;
+  rank: number;
   title: string;
-  image_url: string;
-}
+  images: {
+    jpg: {
+      image_url: string;
+    };
+  };
+  aired: {
+    string: string;
+  };
+  episodes: number;
+  score: number;
+};
 
 const AnimeSearch: React.FC = () => {
   const [searchTerm, setSearchTerm] = useState('');
-  const [animeResults, setAnimeResults] = useState<Anime[]>([]);
+  const [searchResults, setSearchResults] = useState<Anime[]>([]);
 
-  useEffect(() => {
-    const searchAnime = async () => {
-      try {
-        const response = await axios.get(
-          `https://api.jikan.moe/v4/anime?q=${searchTerm}&sfw`
-        );
-        const results: Anime[] = response.data.results;
-        setAnimeResults(results);
-      } catch (error) {
-        console.error('Error searching anime:', error);
-      }
-    };
-
-    const delaySearch = setTimeout(() => {
-      if (searchTerm) {
-        searchAnime();
-      } else {
-        setAnimeResults([]);
-      }
-    }, 500);
-
-    return () => clearTimeout(delaySearch);
-  }, [searchTerm]);
-
-  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const query = event.target.value;
-    setSearchTerm(query);
+  const handleSearch = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.jikan.moe/v4/anime?q=${searchTerm}&sfw`
+      );
+      const { data } = response.data;
+      setSearchResults(data);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
-    <div className='container mx-auto p-4'>
-      <input
-        type='text'
-        placeholder='Search for anime...'
-        className='w-full rounded border border-gray-300 p-2 mb-4'
-        value={searchTerm}
-        onChange={handleSearch}
-      />
-
-      <div className='grid grid-cols-3 gap-4'>
-        {animeResults.map((anime: Anime) => (
-          <div
-            key={anime.mal_id}
-            className='border border-gray-300 rounded p-4'
-          >
-            <img src={anime.image_url} alt={anime.title} className='mb-2' />
-            <p>{anime.title}</p>
-          </div>
-        ))}
+    <div className='px-2'>
+      <h1 className='text-2xl font-bold my-4'>Anime Search</h1>
+      <div className='flex justify-center space-x-4 mb-4'>
+        <input
+          type='text'
+          placeholder='Enter anime name'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className='px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500'
+        />
+        <button
+          onClick={handleSearch}
+          className='px-4 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-500 focus:outline-none focus:bg-indigo-500'
+        >
+          Search
+        </button>
       </div>
+
+      {searchResults.length > 0 ? (
+        <AnimeTable data={searchResults} /> // Render the AnimeTable component with searchResults as data
+      ) : (
+        <p className='text-center'>No results found.</p>
+      )}
     </div>
   );
 };
