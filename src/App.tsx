5cdd69cc5fee6473b9d390279df96b18b68bfd1b
redpangilinan <redpangilinan715@gmail.com>
2023-05-30 21:11:20 +0800

    Added footer and anime details page

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import Home from './pages/Home';
 import AnimeSearch from './pages/AnimeSearch';
+import AnimeDetails from './pages/AnimeDetails'; // Import AnimeDetails component
 
 function App() {
   return (
-    <>
+    <div className='flex flex-col min-h-screen'>
       <Navbar />
-      <div className='container mx-auto'>
+      <div className='flex-grow container mx-auto'>
         <BrowserRouter>
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/search' element={<AnimeSearch />} />
+            <Route path='/anime/:animeId' element={<AnimeDetails />} />
           </Routes>
         </BrowserRouter>
       </div>
-    </>
+      <Footer />
+    </div>
   );
 }
 
