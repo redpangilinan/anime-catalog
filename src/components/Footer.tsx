8ed01169fa94ab929e2a098a30453fcf3d7aef16
redpangilinan <redpangilinan715@gmail.com>
2023-05-30 21:10:36 +0800

    Create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,33 @@
+export default function Footer() {
+  return (
+    <footer className='bg-white rounded-lg border dark:bg-gray-900 mt-4'>
+      <div className='w-full max-w-screen-xl mx-auto p-4 md:py-8'>
+        <div className='sm:flex sm:items-center sm:justify-between'>
+          <a href='/'>
+            <h2 className='text-2xl font-bold'>AnimeCatalog</h2>
+          </a>
+          <ul className='flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400'>
+            <li>
+              <a href='/' className='mr-4 hover:underline md:mr-6 '>
+                Home
+              </a>
+            </li>
+            <li>
+              <a href='/search' className='mr-4 hover:underline md:mr-6'>
+                Search
+              </a>
+            </li>
+          </ul>
+        </div>
+        <hr className='my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8' />
+        <span className='block text-sm text-gray-500 sm:text-center dark:text-gray-400'>
+          © 2023{' '}
+          <a href='/' className='hover:underline'>
+            Anime Catalog
+          </a>
+          . All Rights Reserved.
+        </span>
+      </div>
+    </footer>
+  );
+}
